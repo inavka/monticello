c6a2ada320901aa2f3328e6c31d6f461d8fb716b
inavka <ivanka.zhuravchak@gmail.com>
2019-06-04 19:27:13 +0300

    Vertical slider. Project section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     $('.slick-slider').slick({
         dots: true,
-        arrows: false
-    })
+        arrows: false,
+        vertical: true,
+        verticalSwiping: true
+    });
+
+    $('.sliderNews').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoPlay: true,
+        autoplaySpeed: 4000,
+        dots: true
+    });
 });
+
